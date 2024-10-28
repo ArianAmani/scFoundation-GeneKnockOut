56beb0bd5894a29a4d68beb00647fc5f1b34658c
Arian Amani <arianamani.sec@gmail.com>
2024-10-28 19:28:06 +0100

    Add docstring to __init__

--- scFoundGPert/__init__.py
+++ scFoundGPert/__init__.py
@@ -1,3 +1,21 @@
+"""
+This module initializes the package by importing key functions and classes
+from submodules and defining the public API.
+
+Imports:
+    compute_metrics: Function to compute various metrics.
+    get_metric_confusion_matrices: Function to get confusion matrices for metrics.
+    plot_metrics: Function to plot the computed metrics.
+    scFoundGPert: Class for the scFoundGPert model.
+
+__all__:
+    List of public objects of this module, as interpreted by `import *`.
+    - 'scFoundGPert'
+    - 'compute_metrics'
+    - 'get_metric_confusion_matrices'
+    - 'plot_metrics'
+"""
+
 from ._utils import (
     compute_metrics,
     get_metric_confusion_matrices,
